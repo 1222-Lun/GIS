501df3e4d4b2bfb02134580fec28cad5d5efa1e0
1222-Lun <72474226+1222-Lun@users.noreply.github.com>
2021-01-11 23:50:55 +0000

    Total death cases distribution of the US

--- US/US-Death.R
+++ US/US-Death.R
@@ -1,3 +1,4 @@
+#load the R packages
 library(tidyverse)
 library(sf)
 library(tmap)
@@ -5,19 +6,28 @@ library(spData)
 library(viridis)
 library(lubridate)
 
+
+# Read the CSV file of the confirmed death cases
 Death<- read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv")
+# Select the newest data when I write the essay
 Death_Newest<- dplyr::select(Death, `Province_State`, `Country_Region`, Lat, Long_,`1/7/21`)
 
+#Select the updated data of the US
 US_DeathNewest <- Death_Newest %>% 
   filter(`Country_Region`=="US") %>% 
   select(-`Country_Region`) %>% 
   rename(State=`Province_State`, Death=`1/7/21`)
 
+#Load the US states geographic data from the package spData
 data(us_states)
 cov_USDeath <- left_join(us_states, US_DeathNewest, by = c("NAME" = "State"))
 nrow(cov_USDeath)
 
+#Set the breaks
 breaks <- c(0, 100, 16000)
+
+
+#use tmap functions to plot the map
 tm_shape(cov_USDeath) +
   tm_polygons(border.col="white", 
               lwd=2, 
