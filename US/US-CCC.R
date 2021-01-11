b97a714d36d7b4b9d7a17dfb10cfb6a6fa600d09
1222-Lun <72474226+1222-Lun@users.noreply.github.com>
2021-01-11 23:46:37 +0000

    Cumulative confirmed death cases in the US

--- US/US-CCC.R
+++ US/US-CCC.R
@@ -1,3 +1,5 @@
+
+#load the R packages
 library(tidyverse)
 library(sf)
 library(tmap)
@@ -5,19 +7,27 @@ library(spData)
 library(viridis)
 library(lubridate)
 
+
+# Read the CSV file of the confirmed cases
 Confirmed<- read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv")
+# Select the newest data when I write the essay
 Confirmed_Newest<- dplyr::select(Confirmed, `Province_State`, `Country_Region`, Lat, Long_,`1/7/21`)
 
+#Select the updated data of the US
 US_Newest <- Confirmed_Newest %>% 
   filter(`Country_Region`=="US") %>% 
   select(-`Country_Region`) %>% 
   rename(State=`Province_State`, Confirmed=`1/7/21`)
 
+#Load the us states geographic data from the package spData
 data(us_states)
 cov_US <- left_join(us_states, US_Newest, by = c("NAME" = "State"))
 nrow(cov_US)
 
+#Set the breaks
 breaks <- c(10, 5000, 800000)
+
+##use tmap functions to plot the map
 tm_shape(cov_US) +
   tm_polygons(border.col="white", 
               lwd=2, 
