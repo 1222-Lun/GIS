b2b620f64e1e0212e6430a5585a86aff7cafd8ee
1222-Lun <72474226+1222-Lun@users.noreply.github.com>
2021-01-11 23:58:37 +0000

    World map with global COVID data
    The data details can be changed to obtain different maps from different time period

--- Global/Global-1.R
+++ Global/Global-1.R
@@ -1,20 +1,26 @@
+#load packages
 library(tidyverse)
 library(ggplot2)
 library(maps)
 library(viridis)
 library(readr)
 
+#Read the CSV file
 Confirmed <- read_csv(url("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"))
 
+#Check the updated information
 select(Confirmed,tail(names(Confirmed),1))
 
+#load the worldmap
 world <-map_data("world")
 ggplot() +geom_polygon(data=world, aes(x=long, y=lat, group=group), fill="grey")
 
+#Plot the map with COVID data
 ggplot()+
   geom_polygon(data=world,aes(x=long,y=lat,group=group),fill="grey",alpha=0.3) +
   geom_point(data=Confirmed,aes(x=Long,y=Lat,size=`2/7/20`,color=`2/7/20`),alpha=0.5)
 
+# Use ggplot Func to plot the map
 mybreaks<- c(1, 20, 100, 1000, 50000)
 mylabels<- c("1-19", "20-99", "100-999","1,000-49,999", "50000+")
 ggplot() +
